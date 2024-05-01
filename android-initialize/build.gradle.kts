9a11f4fb469e9a2ba9f521b339db8612dfaab59e
丁笛 <466820812@qq.com>
2024-05-01 11:59:41 +0800

    发布JitPack

--- android-initialize/build.gradle.kts
+++ android-initialize/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +41,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                // 应用 release 构建变体的组件
+                from(components["release"])
+
+                // 然后，你可以如下方式自定义发布的属性
+                groupId = "com.dingdi.initialize"
+                artifactId = "dd"
+                version = "1.0.0"
+            }
+        }
+    }
 }
